4dfaf2963d4fcb7ff7025ef552430e73deeeb9d5
M W <starry_211534@outlook.com>
2026-09-08 20:46:29 +0800

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://menu.matrixqr.cc/', 
+  generateRobotsTxt: true, 
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000, 
+};
